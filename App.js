6e191e5512ccfeab2b06351f8c5e5eaae2659791
gs-rmedidha <rmedidha@gainsight.com>
2025-02-14 11:07:17 +0530

    first episode

--- App.js
+++ App.js
@@ -1,3 +1,17 @@
-const heading = React.createElement('h1',{},'Welcome to React!');
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+window.onload = function(){
+    const heading = React.createElement('div',{
+        "title":'Header Title',
+        "id":"parent"
+    }, [
+        React.createElement('div',{id:'child'},
+            [React.createElement('h1',{},"I am H1 tag"),React.createElement('h2',{},"I am H2 tag")]
+        ),
+        React.createElement('div',{id:'child2'},
+            [React.createElement('h1',{},"I am H1 tag"),React.createElement('h2',{},"I am H2 tag")]
+        )
+    ]);
+    console.log(heading);
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    console.log(root);
+    root.render(heading);
+}
